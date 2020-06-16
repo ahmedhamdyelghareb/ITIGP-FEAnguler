3ffed0ab9f41844028b9341d5f1bf03fea39de02
Ahmed Hamdy Elghareb <48691972+ahmedhamdyelghareb@users.noreply.github.com>
2020-06-16 02:53:23 +0200

    shopform validation from client

--- src/app/components/shop-login/shop-login.component.ts
+++ src/app/components/shop-login/shop-login.component.ts
@@ -18,29 +18,29 @@ export class ShopLoginComponent implements OnInit {
   }
 
   ShopRegForm=new FormGroup({
-    FName:new FormControl('',Validators.nullValidator),
-    LName:new FormControl('',Validators.nullValidator),
-    email:new FormControl('',Validators.nullValidator),
-    password:new FormControl('',Validators.nullValidator),
-    phone:new FormControl('',),
-    shopname:new FormControl('',),
-    agree:new FormControl('',),
-    address:new FormControl('',),
+    FName:new FormControl('',[Validators.required]),
+    LName:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.pattern("[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}")]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+    phone:new FormControl('',[Validators.required,Validators.minLength(11)]),
+    shopname:new FormControl('',[Validators.required]),
+    agree:new FormControl('',[Validators.required]),
+    address:new FormControl('',[Validators.required]),
     type:new FormControl('shop')
   })
 
   onSubmit(){
-    console.log(this.ShopRegForm.value.FName)
-    this.user.addShop(
-      this.ShopRegForm.value.FName,
-      this.ShopRegForm.value.LName,
-      this.ShopRegForm.value.email,
-      this.ShopRegForm.value.phone,
-      this.ShopRegForm.value.password,
-      this.ShopRegForm.value.Type,
-      this.ShopRegForm.value.shopName,
-      ).subscribe(res => {
-        console.log(res)
-      });
+    console.log(this.ShopRegForm.value)
+    // this.user.addShop(
+    //   this.ShopRegForm.value.FName,
+    //   this.ShopRegForm.value.LName,
+    //   this.ShopRegForm.value.email,
+    //   this.ShopRegForm.value.phone,
+    //   this.ShopRegForm.value.password,
+    //   this.ShopRegForm.value.Type,
+    //   this.ShopRegForm.value.shopName,
+    //   ).subscribe(res => {
+    //     console.log(res)
+    //   });
   }
 }

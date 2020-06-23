26e90eefa72010bf449782f8d9a413ed905129b7
yomna334 <yomnaayman987@gmail.com>
2020-06-23 06:47:23 +0200

    update

--- src/app/components/store/product-add/product-add.component.ts
+++ src/app/components/store/product-add/product-add.component.ts
@@ -5,115 +5,161 @@ import { Product } from 'src/app/Models/product.model';
 import { Subscription } from 'rxjs';
 import { Router,ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpEventType } from '@angular/common/http';
-import { NgForm } from '@angular/forms';
+import { FormGroup,NgForm,Validators,FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css']
 })
+
 export class ProductAddComponent implements OnInit {
+  fileData: File = null;
+  previewUrl:any = null;
+  fileUploadProgress: string = null;
+  uploadedFilePath: string = null;
+  productForm:NgForm;
+    constructor(private http:HttpClient,public productService:ProductService ,private router: Router) {
+
+     }
+
+
+    //  ngOnInit() {
+
+    //  }
+
+// fileProgress(fileInput: any) {
+//       this.fileData = <File>fileInput.target.files[0];
+//       this.preview();
+// }
+
+// preview() {
+//     // Show preview
+//     var mimeType = this.fileData.type;
+//     if (mimeType.match(/image\/*/) == null) {
+//       return;
+//     }
+
+//     var reader = new FileReader();
+//     reader.readAsDataURL(this.fileData);
+//     reader.onload = (_event) => {
+//       this.previewUrl = reader.result;
+//     }
+// }
+
+//      selectedFile:File=null;
+
+//     onFileSelected(event){
+//       this.selectedFile=<File>event.target.files[0];
+//     }
+//     onUpload(){
+//   const fd=new FormData();
+//   fd.append('image',this.selectedFile,this.selectedFile.name);
+//   this.http.post('http://localhost:5000/api/store/create',fd,{
+//     reportProgress:true,
+//     observe:'events'
+//   }).subscribe(event=>{
+//     if(event.type === HttpEventType.UploadProgress){
+//       console.log('uploadProgress' + Math.round(event.loaded/event.total *100) +'%');
+//     }else if(event.type === HttpEventType.Response){
+//     console.log(event);
+//     }
+//   });
+//     }
+//     id:number;
+//     Product: Product = {
+//       title: "",
+//       price: 0,
+//       imageUrl:"",
+//       id:0,
+//       description:"",
+//       amount:0
+//     }
+//     product: Product[] = [];
+
+// //     onFileSelected(event){
+// //           if(event.target.files.length >0)
+// //           {
+// // const file= event.target.files
+// //           }
+// //         }
+//     onAddNewProduct(form: NgForm){
+//       const formData = new FormData();
+//       formData.append('imageUrl', this.fileData);
+//       this.http.post('http://localhost:5000/api/store/create', formData, {
+//         reportProgress: true,
+//         observe: 'events'
+//       })
+//       .subscribe(events => {
+//         if(events.type === HttpEventType.UploadProgress) {
+//           this.fileUploadProgress = Math.round(events.loaded / events.total * 100) + '%';
+//           console.log(this.fileUploadProgress);
+//         } else if(events.type === HttpEventType.Response) {
+//           this.fileUploadProgress = '';
+//           console.log(events.body);
+//           alert('SUCCESS !!');
+//         }
+
+//       }) ;
+//       this.productService.addProduct(
+//         form.value.title,
+//         form.value.price,
+//         form.value.imageUrl,
+//         form.value.description,
+//         form.value.amount,
+//         ).subscribe(res => {
+//           console.log("done")
+//           console.log("added")
+//       });
+//       console.log("yees")
+//       form.resetForm();
+//         }
+
+
+
+
+ngOnInit() {
+}
+prp;
+img;
+
+onAddNewProduct(product){
+  const formData = new FormData();
+
+  formData.append('imageUrl',this.img);
+  formData.append('title',product.title);
+  formData.append('description',product.description);
+  formData.append('price',product.price);
+  formData.append('amount',product.amount)
+
+  this.productService.addProduct(formData).subscribe(data => {
+    this.prp = data;
+  })
+
+  alert("Product Added");
+  this.router.navigate(['store/products']);
+}
+
+//Image Previewing Section
+imagePreview ;
+onImagePicked(val){
+  this.img = val.target.files[0]
+
+  const file = (val.target as HTMLInputElement).files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    this.imagePreview = reader.result;
+  };
+  reader.readAsDataURL(file);
+
+}
 
 
 
 
 
- selectedProduct:Product={
-   id:0,
-   title:"",
-   price:0,
-   imageUrl:"",
-   description:"",
-   amount:0
- };
-  constructor(private http:HttpClient,public productService:ProductService ,private router: Router) {
-
-   }
-
-   private productsSub: Subscription;
-   ngOnInit() {
-     this.getAllProducts()
-   }
- id
-   products :Product []
-   fetechedProducts : Product[]
-   getAllProducts(){
-    this.productsSub= this.productService.getProducts()
-     .subscribe(
-       (productData:[])=>{
-         this.fetechedProducts =this.products=productData
-       },
-       (err)=>{
-         console.log(err);
-       }
-     )
-   }
-   selectedFile:File=null;
-  filter(query:string){
-    console.log(query);
-  }
-  onFileSelected(event){
-    this.selectedFile=<File>event.target.files[0];
-  }
-  onUpload(){
-const fd=new FormData();
-fd.append('image',this.selectedFile,this.selectedFile.name);
-this.http.post('http://localhost:5000/api/store/create',fd,{
-  reportProgress:true,
-  observe:'events'
-}).subscribe(event=>{
-  if(event.type === HttpEventType.UploadProgress){
-    console.log('uploadProgress' + Math.round(event.loaded/event.total *100) +'%');
-  }else if(event.type === HttpEventType.Response){
-  console.log(event);
-  }
-});
-  }
-//new product
-
-  onAddNewProduct(form:NgForm){
-if(this.selectedProduct.id==0){
-    this.productService.addProduct(
-      form.value.title,
-      form.value.price,
-      form.value.imageUrl,
-      form.value.description,
-      form.value.amount,
-      ).subscribe(res => {
-        console.log("done")
-        console.log("added")
-    });
-  }else{
-this.productService.updateProduct(this.selectedProduct.id,this.selectedProduct).subscribe(res=>{
-  console.log("updated")
-  console.log(this.selectedProduct)
-})
-  }
-    form.resetForm();
-      }
 
 
-
-
-    onDeleteProduct(id){
-      this.productService.deleteProduct(id).subscribe(data=>{
-        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-          this.router.navigate(['/store/products']);
-        });
-      })
 
     }
-    getProductById(product:Product){
-      this.selectedProduct.id=product.id;
-      this.selectedProduct.title=product.title;
-      this.selectedProduct.price=product.price;
-      this.selectedProduct.imageUrl=product.imageUrl;
-      this.selectedProduct.description=product.description;
-      this.selectedProduct.amount=product.amount;
-      console.log(this.selectedProduct)
-
-    }
-
-  }
-
 
--- src/app/components/store/products/products.component.ts
+++ src/app/components/store/products/products.component.ts
@@ -18,14 +18,14 @@ export class ProductsComponent implements OnInit {
   @ViewChild('editProductForm',{static: false}) editForm: NgForm;
 
 
- selectedProduct:Product={
-   id:0,
-   title:"",
-   price:0,
-   imageUrl:"",
-   description:"",
-   amount:0
- };
+//  selectedProduct:Product={
+//   //  id:0,
+//    title:"",
+//    price:0,
+//   //  imageUrl:"",
+//    description:"",
+//    amount:0
+//  };
   constructor(private http:HttpClient,public productService:ProductService ,private router: Router) {
 
    }
@@ -49,9 +49,11 @@ export class ProductsComponent implements OnInit {
      )
    }
    selectedFile:File=null;
-  filter(query:string){
-    console.log(query);
-  }
+   filter(query:string){
+    this.fetechedProducts = (query) ?
+    this.products.filter(p=>p.title.toLowerCase().includes(query.toLowerCase())) :
+    this.products
+   }
   onFileSelected(event){
     this.selectedFile=<File>event.target.files[0];
   }
@@ -99,46 +101,46 @@ this.http.post('http://localhost:5000/api/store/create',fd,{
 
 
     }
-    getProductById(product:Product){
-      this.selectedProduct.id=product.id;
-      this.selectedProduct.title=product.title;
-      this.selectedProduct.price=product.price;
-      this.selectedProduct.imageUrl=product.imageUrl;
-      this.selectedProduct.description=product.description;
-      this.selectedProduct.amount=product.amount;
-      console.log(this.selectedProduct)
-      this.editForm.form.patchValue({
-        id : this.selectedProduct.id,
-        title : this.selectedProduct.title,
-        price : this.selectedProduct.price,
-        imageUrl : this.selectedProduct.imageUrl,
-        description : this.selectedProduct.description,
-      })
-    }
+    // getProductById(product:Product){
+    //   this.selectedProduct.id=product.id;
+    //   this.selectedProduct.title=product.title;
+    //   this.selectedProduct.price=product.price;
+    //   this.selectedProduct.imageUrl=product.imageUrl;
+    //   this.selectedProduct.description=product.description;
+    //   this.selectedProduct.amount=product.amount;
+    //   console.log(this.selectedProduct)
+    //   this.editForm.form.patchValue({
+    //     // id : this.selectedProduct.id,
+    //     title : this.selectedProduct.title,
+    //     price : this.selectedProduct.price,
+    //     imageUrl : this.selectedProduct.imageUrl,
+    //     description : this.selectedProduct.description,
+    //   })
+    // }
 
-    onSubmitEdit(){
-      this.selectedProduct.id = this.editForm.value.id;
-      this.selectedProduct.title = this.editForm.value.title;
-      this.selectedProduct.price = this.editForm.value.price;
-      this.selectedProduct.imageUrl = this.editForm.value.imageUrl;
-      this.selectedProduct.description = this.editForm.value.description;
-      this.selectedProduct.amount = this.editForm.value.amount;
+    // onSubmitEdit(){
+    //   // this.selectedProduct.id = this.editForm.value.id;
+    //   this.selectedProduct.title = this.editForm.value.title;
+    //   this.selectedProduct.price = this.editForm.value.price;
+    //   this.selectedProduct.imageUrl = this.editForm.value.imageUrl;
+    //   this.selectedProduct.description = this.editForm.value.description;
+    //   this.selectedProduct.amount = this.editForm.value.amount;
 
 
-      console.log(this.selectedProduct);
+    //   console.log(this.selectedProduct);
 
-      this.productService.updateProduct(
-        this.selectedProduct.id,
-        this.selectedProduct
-      )
-        .subscribe(()=> {
-          this.getAllProducts();
-          console.log("Product Editted")
-        }, (err)=>{
-          console.log(err)
-        })
+    //   this.productService.updateProduct(
+    //     this.selectedProduct.id,
+    //     this.selectedProduct
+    //   )
+    //     .subscribe(()=> {
+    //       this.getAllProducts();
+    //       console.log("Product Editted")
+    //     }, (err)=>{
+    //       console.log(err)
+    //     })
 
-    }
+    // }
 
 
   }

4883ce2d1fa2c5071b4da6f6963c584c71e807b6
Ahmed Hamdy Elghareb <48691972+ahmedhamdyelghareb@users.noreply.github.com>
2020-06-24 03:33:57 +0200

    add catigiry fix delete

--- src/app/components/store/product-add/product-add.component.ts
+++ src/app/components/store/product-add/product-add.component.ts
@@ -24,100 +24,6 @@ export class ProductAddComponent implements OnInit {
      }
 
 
-    //  ngOnInit() {
-
-    //  }
-
-// fileProgress(fileInput: any) {
-//       this.fileData = <File>fileInput.target.files[0];
-//       this.preview();
-// }
-
-// preview() {
-//     // Show preview
-//     var mimeType = this.fileData.type;
-//     if (mimeType.match(/image\/*/) == null) {
-//       return;
-//     }
-
-//     var reader = new FileReader();
-//     reader.readAsDataURL(this.fileData);
-//     reader.onload = (_event) => {
-//       this.previewUrl = reader.result;
-//     }
-// }
-
-//      selectedFile:File=null;
-
-//     onFileSelected(event){
-//       this.selectedFile=<File>event.target.files[0];
-//     }
-//     onUpload(){
-//   const fd=new FormData();
-//   fd.append('image',this.selectedFile,this.selectedFile.name);
-//   this.http.post('http://localhost:5000/api/store/create',fd,{
-//     reportProgress:true,
-//     observe:'events'
-//   }).subscribe(event=>{
-//     if(event.type === HttpEventType.UploadProgress){
-//       console.log('uploadProgress' + Math.round(event.loaded/event.total *100) +'%');
-//     }else if(event.type === HttpEventType.Response){
-//     console.log(event);
-//     }
-//   });
-//     }
-//     id:number;
-//     Product: Product = {
-//       title: "",
-//       price: 0,
-//       imageUrl:"",
-//       id:0,
-//       description:"",
-//       amount:0
-//     }
-//     product: Product[] = [];
-
-// //     onFileSelected(event){
-// //           if(event.target.files.length >0)
-// //           {
-// // const file= event.target.files
-// //           }
-// //         }
-//     onAddNewProduct(form: NgForm){
-//       const formData = new FormData();
-//       formData.append('imageUrl', this.fileData);
-//       this.http.post('http://localhost:5000/api/store/create', formData, {
-//         reportProgress: true,
-//         observe: 'events'
-//       })
-//       .subscribe(events => {
-//         if(events.type === HttpEventType.UploadProgress) {
-//           this.fileUploadProgress = Math.round(events.loaded / events.total * 100) + '%';
-//           console.log(this.fileUploadProgress);
-//         } else if(events.type === HttpEventType.Response) {
-//           this.fileUploadProgress = '';
-//           console.log(events.body);
-//           alert('SUCCESS !!');
-//         }
-
-//       }) ;
-//       this.productService.addProduct(
-//         form.value.title,
-//         form.value.price,
-//         form.value.imageUrl,
-//         form.value.description,
-//         form.value.amount,
-//         ).subscribe(res => {
-//           console.log("done")
-//           console.log("added")
-//       });
-//       console.log("yees")
-//       form.resetForm();
-//         }
-
-
-
-
 ngOnInit() {
 }
 prp;
@@ -125,12 +31,13 @@ img;
 
 onAddNewProduct(product){
   const formData = new FormData();
-
+  console.log(formData)
   formData.append('imageUrl',this.img);
   formData.append('title',product.title);
   formData.append('description',product.description);
   formData.append('price',product.price);
   formData.append('amount',product.amount);
+  formData.append('category',product.category);
 
   this.productService.addProduct(formData).subscribe(data => {
     this.prp = data;
